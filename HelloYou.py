450666ff8d8e2663dbe76ff26bbbf87a764a8c5e
MatthewWillems <70891159+MatthewWillems@users.noreply.github.com>
2020-10-29 21:58:37 +0100

    Add files via upload

---
+++ HelloYou.py
@@ -0,0 +1,4 @@
+print ("Hello You!, ik ben Matthew.")
+print ("Wie ben jij?")
+naam = input()
+print ("Hello " + naam)
---
+++ keuzeverhaal.py
@@ -0,0 +1,51 @@
+print("Waar woon ik?")
+print("A = Haarlem")
+print("B = Amsterdam")
+print("C = Zaandam")
+choice = input()
+if choice == 'A':
+    print("Nee. Ik woon in Zaandam")
+elif choice == 'B':
+    print("Nee. Ik woon in Zaandam")
+elif choice == 'C':
+    print("Ja! Ik woon inderdaad in Zaandam.")
+
+print("Heb ik broers of zussen?")
+print("A = Nee.")
+print("B = Ja.")
+choice = input()
+if choice == 'A':
+    print("Dit klopt. Ik ben enigskind.")
+elif choice == 'B':
+    print("Nee. Ik heb geen broers of zussen")
+
+print("Heb ik huisdieren?")
+print("A = Ja.")
+print("B = Nee.")
+choice = input()
+if choice == 'A':
+    print("Ja! Ik heb een kat genaamd Max.")
+elif choice == 'B':
+    print("Ik heb wel een huisdier, namelijk een kat genaamd Max.")
+
+print("Heb ik hobby's?")
+print("A = Nee.")
+print("B = Ja.")
+choice = input()
+if choice == 'A':
+    print("Klopt. Ik heb wel veel intresses, maar ik zou dit niet hobby's kunnen noemen.")
+elif choice == 'B':
+    print("Nee. Ik heb wel veel intresses, maar ik zou dit niet hobby's kunnen noemen.")
+
+
+print("Wat is mijn achternaam?")
+print("A = Willems")
+print("B = van de Pol")
+print("C = Groenhoff")
+choice = input()
+if choice == 'A':
+    print("Klopt!")
+elif choice == 'B':
+    print("Nope.")
+elif choice == 'C':
+    print("Nope.")
